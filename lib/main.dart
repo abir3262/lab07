9a93ecb20537a4ed9ab8f40c5df261084f393456
abir3262 <hasan15-5459@diu.edu.bd>
2025-03-11 10:09:31 +0600

    iki

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'screen/home/page/home.dart';
+
+void main() {
+  runApp(const FreebeesEcom());
+}
+
+class FreebeesEcom extends StatelessWidget {
+  const FreebeesEcom({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Home(),
+    );
+  }
+}
